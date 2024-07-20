5927d657795798c9da4153302ea18a7e7fca6ae8
FeiYan <yan00126@algonquinlive.com>
2024-07-20 19:12:20 -0400

    complete

---
+++ Item.cs
@@ -0,0 +1,16 @@
+﻿namespace ToDoList;
+
+public class Item (string task)
+{
+    // public string task; //field
+    // private bool isComplete = false; //field
+
+    public string Task { get; set;} = task; //property
+
+    public bool IsComplete {get; set; } = false;// property
+
+    public void MarkTask ()
+    {
+        IsComplete = true;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,33 +1,105 @@
-﻿Console.WriteLine("Welcome to Your To Do List.");
-bool isActive = true;
+﻿using System.Security.AccessControl;
+using ToDoList;
 
-while (isActive) 
+internal class Program
 {
-  Console.Write("\nChoose an action (Add, View, Mark, or Exit): ");
-  string? action = Console.ReadLine();
+    private static List<Item> items = [];
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Your To Do List.");
+        bool isActive = true;
 
-  if (string.IsNullOrEmpty(action)) {
-    Console.WriteLine("\nNot a valid response.");
-    continue;
-  }
+        while (isActive)
+        {
+            Console.Write("\nChoose an action (Add, View, Mark, or Exit): ");
+            string? action = Console.ReadLine();
 
-  switch (action.ToLower())
-  {
-    case "add":
-      // Add a new item
-      break;
-    case "view":
-      // Display all items
-      break;
-    case "mark":
-      // Mark an item as complete
-      break;
-    case "exit":
-      isActive = false;
-      Console.WriteLine("\nGoodbye");
-      break;
-    default: 
-      Console.WriteLine("\nNot a valid response.");
-      break;
-  }
+            if (string.IsNullOrEmpty(action))
+            {
+                Console.WriteLine("\nNot a valid response.");
+                continue;
+            }
+
+            switch (action.ToLower())
+            {
+                case "add":
+                    // Add a new item
+                    AddItem();
+                    break;
+                case "view":
+                    // Display all items
+                    DisplayItems();
+                    break;
+                case "mark":
+                    // Mark an item as complete
+
+                    MarkItem();
+                    break;
+                case "exit":
+                    isActive = false;
+                    Console.WriteLine("\nGoodbye");
+                    break;
+                default:
+                    Console.WriteLine("\nNot a valid response.");
+                    break;
+            }
+        }
+    }
+
+    private static void AddItem ()
+    {
+      string? task = null;
+      Item item;
+
+      while (string.IsNullOrEmpty(task))
+      {
+        Console.Write("\nProvide the task for the new item: ");
+        task = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(task))
+        {
+          // create new item object
+          item = new Item(task);
+
+          //set the task for the item
+          // item.Task = task;
+          // add the item to the list
+          items.Add(item);
+
+          Console.WriteLine("An item has been added.");
+
+        }
+      }
+    }
+    private static void DisplayItems()
+    {
+      Console.WriteLine();
+      for (int i = 0; i < items.Count; i++)
+      {
+        Console.WriteLine($"{i} - {(items[i].IsComplete ? "x" : " ")} {items[i].Task}");
+
+      }
+    }
+
+    private static void MarkItem ()
+    {
+      while (true)
+      {
+        Console.Write("\nProvide the index of the item to mark complete:");
+        string? input = Console.ReadLine();
+        bool isValid = int.TryParse(input, out int index);
+
+        if (isValid)
+        {
+          items[index].MarkTask();
+          Console.WriteLine("Item has been marked complete");
+          return;
+
+
+        }
+
+        Console.WriteLine("Not a valid response");
+
+      }
+    }
 }
